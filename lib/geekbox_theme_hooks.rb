e69aca477dc9ba56f563a2a4ad7038820d478d18
Alexander Kirillov <saratovsource@gmail.com>
2011-06-28 12:13:24 +0400

    Продолжаю делать user-panel

--- lib/geekbox_theme_hooks.rb
+++ lib/geekbox_theme_hooks.rb
@@ -1,4 +1,5 @@
 class GeekboxThemeHooks < Spree::ThemeSupport::HookListener
   # custom hooks go here
   insert_before :main, 'shared/action_bar/bar'
+  insert_after :cart_form, 'products/compare'
 end
